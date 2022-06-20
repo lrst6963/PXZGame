d0f4f1e8a24b1e4b046a7ac638609fa14a16621d
Lrst6963 <3244255153@qq.com>
2022-06-20 14:13:09 +0800

    last commit

---
+++ PXZ/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace PXZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Arial", 50F);
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 100);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Arial", 50F);
+            this.label2.Location = new System.Drawing.Point(100, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 100);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Red;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Arial", 50F);
+            this.label3.Location = new System.Drawing.Point(200, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 100);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Green;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Arial", 50F);
+            this.label4.Location = new System.Drawing.Point(0, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 100);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "4";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Green;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Arial", 50F);
+            this.label5.Location = new System.Drawing.Point(100, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 100);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "5";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Green;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Font = new System.Drawing.Font("Arial", 50F);
+            this.label6.Location = new System.Drawing.Point(200, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 100);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "6";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Blue;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Font = new System.Drawing.Font("Arial", 50F);
+            this.label7.Location = new System.Drawing.Point(0, 200);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 100);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "7";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Blue;
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Font = new System.Drawing.Font("Arial", 50F);
+            this.label8.Location = new System.Drawing.Point(100, 200);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 100);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "8";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label8.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.Blue;
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Font = new System.Drawing.Font("Arial", 50F);
+            this.label9.Location = new System.Drawing.Point(200, 200);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(100, 100);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "9";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label9.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 20F);
+            this.button1.Location = new System.Drawing.Point(40, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(215, 39);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(300, 381);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ PXZ/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PXZ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Label[,] arrLbl = new Label[3, 3];
+        int unRow = 0, unCol = 0;
+        bool playing = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Play")
+            {
+                button1.Text = "Pause";
+                this.Text = "Game Playing...";
+                rmblock();
+            }
+            else if (button1.Text == "Pause")
+            {
+                button1.Text = "Continue";
+                this.Text = "Game Pauseing...";
+                gpause();
+            }
+            else if (button1.Text == "Continue") 
+            {
+                this.Text = "Game Playing...";
+                button1.Text = "Pause";
+                gcontinue();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (!playing) 
+            {
+                return;
+            }
+            int row = ((Label)sender).Top / 100;
+            int col = ((Label)sender).Left / 100;
+            if (Math.Abs(row - unRow) + Math.Abs(col - unCol) == 1)
+            {
+                string temp = arrLbl[unRow, unCol].Text;        //存根字符
+                Color temps = arrLbl[unRow, unCol].BackColor;   //存根颜色
+
+                arrLbl[unRow, unCol].BackColor = arrLbl[row, col].BackColor;
+                arrLbl[unRow, unCol].Text = arrLbl[row, col].Text;
+
+                arrLbl[row, col].Text = temp;
+                arrLbl[row, col].BackColor = temps;
+
+                arrLbl[unRow, unCol].Visible = true;
+                arrLbl[row, col].Visible = false;
+                unRow = row;
+                unCol = col;
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                if (arrLbl[i / 3, i % 3].Text != Convert.ToString(i + 1))
+                {
+                    break;
+                }
+                if (i == 8)
+                {
+                    arrLbl[unRow, unCol].Visible = true;
+                    playing = false;
+                    MessageBox.Show("Game Over!");
+                    button1.Text = "Play";
+                }
+            }
+        }
+
+        public void rmblock() 
+        {
+            arrLbl1();
+            arrLbl[unRow, unCol].Visible = true;
+            int[] arrNum = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Random rm = new Random();
+            for (int i = 0; i < 8; i++)
+            {
+                int rmNum = rm.Next(i, 9);
+                int temp = arrNum[i];
+                arrNum[i] = arrNum[rmNum];
+                arrNum[rmNum] = temp;
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                arrLbl[i / 3, i % 3].Text = arrNum[i].ToString();
+                //     1/3=0  1%3=0
+                //     2/3=0  2%3=1....
+            }
+            int cover = rm.Next(0, 9);
+            unRow = cover / 3;
+            unCol = cover % 3;
+            arrLbl[unRow, unCol].Visible = false; //随机一个label隐藏
+            playing = true;
+        }
+
+        public void arrLbl1() 
+        {
+            arrLbl[0, 0] = label1;
+            arrLbl[0, 1] = label2;
+            arrLbl[0, 2] = label3;
+            arrLbl[1, 0] = label4;
+            arrLbl[1, 1] = label5;
+            arrLbl[1, 2] = label6;
+            arrLbl[2, 0] = label7;
+            arrLbl[2, 1] = label8;
+            arrLbl[2, 2] = label9;
+        }
+
+        public void gpause() 
+        {
+            arrLbl1();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    arrLbl[i, j].Enabled = false;
+                }
+            }
+        }
+
+        public void gcontinue()
+        {
+            arrLbl1();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    arrLbl[i, j].Enabled = true;
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.F5)
+            {
+                button1.Text = "Pause";
+                this.Text = "Game Playing...";
+                rmblock();
+                gcontinue();
+            }
+            else if (e.KeyData == Keys.Escape)
+            {
+                System.Environment.Exit(0);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "";
+        }
+
+    }
+}
